cecdfe30e2817235adefb3372d9a8f77ea6199ce
jonathanOviedo21 <daivisjhony@gmail.com>
2021-10-15 13:45:37 -0300

    crear perro

---
+++ ejercicio4Github/src/ejercicio4github/Perro.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio4github;
+
+/**
+ *
+ * @author jony
+ */
+public class Perro {
+    private String nombre;
+    private int edad;
+    private String raza;
+    private double peso;
+    private double altura;
+    
+    
+}
